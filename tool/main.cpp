c1f6a4534a1d9f632abe3f0253c0a9b82a397982
Dan Liew <daniel.liew@imperial.ac.uk>
2014-05-23 14:14:34 +0100

    Added demonstration of linking with external library Boost program_options

--- tool/main.cpp
+++ tool/main.cpp
@@ -1,10 +1,40 @@
 #include "mylib/random.h"
+#include <boost/program_options.hpp>
 #include <iostream>
 
 using namespace std;
 
+namespace po = boost::program_options;
+
 int main(int argn, char** argv)
 {
+    // This example code was ripped straight from
+    // https://github.com/boostorg/program_options/blob/develop/example/first.cpp
+
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("compression", po::value<int>(), "set compression level")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argn, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    if (vm.count("compression")) {
+        cout << "Compression level was set to "
+     << vm["compression"].as<int>() << ".\n";
+    } else {
+        cout << "Compression level was not set.\n";
+    }
+
+   // Finish  Boost program_options stuff
    cout << "Hello world!" << endl;
 
    cout << "Did you know, I can generated random numbers?" << endl;
